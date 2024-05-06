026c6754862875abc1abb2d2d6197654f6769dbd
Vivien S <73798748+viviensiu@users.noreply.github.com>
2024-05-06 16:21:02 +0100

    Add files via upload
    updated references.py

--- pages/7_📜_References.py
+++ pages/7_📜_References.py
@@ -1,4 +1,30 @@
 import streamlit as st
-from page_utils import font_modifier, display_image
+#from page_utils import font_modifier, display_image
 
-display_image.display_image('https://cdn-images-1.medium.com/max/800/0*vBDO0wwrvAIS5e1D.png')
+#display_image.display_image('https://cdn-images-1.medium.com/max/800/0*vBDO0wwrvAIS5e1D.png')
+
+
+st.title("References")
+
+# Page 3
+st.subheader("Liberia Statistics")
+st.markdown("[World Bank Data catalog](https://datacatalog.worldbank.org/home)")
+st.markdown("[The DHS Program](https://dhsprogram.com/)")
+st.markdown("[The Geospatial Covariate Datasets Manual from DHS Program](https://spatialdata.dhsprogram.com/references/DHS_Covariates_Extract_Data_Description_3.pdf)")
+st.markdown("[Guide to DHS Statistics](https://dhsprogram.com/Data/Guide-to-DHS-Statistics/index.cfm)")
+st.markdown("[Humanitarian Data Exchange(HDX) Liberia Health Facilities](https://data.humdata.org/dataset/hotosm_lbr_health_facilities)")
+st.divider()
+
+# Page 4
+st.subheader("Rainfall, Intervention and Malaria statistics in Liberia")
+st.markdown("[Malaria Atlas Project](https://malariaatlas.org/)") 
+st.markdown("[Humanitarian Data Exchange(HDX) Liberia Rainfall Indicators at Subnational Level](https://data.humdata.org/dataset/a1f60b8a-51ff-4ee7-87ab-897983714595)")
+
+st.divider()
+
+# Page 5
+st.subheader("Malaria Impact on Liberia")
+st.markdown("[Malaria Atlas Project](https://malariaatlas.org/)") 
+
+
+
