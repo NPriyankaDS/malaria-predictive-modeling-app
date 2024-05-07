3a3aaa80b52686c27e00f8740bffcd0612443340
NPriyankaDS <nprinka235@gmail.com>
2024-05-07 14:32:16 +0530

    Did the changes to the malaria prediction application

--- pages/7_📜_References.py
+++ pages/7_📜_References.py
@@ -4,21 +4,21 @@ import streamlit as st
 #display_image.display_image('https://cdn-images-1.medium.com/max/800/0*vBDO0wwrvAIS5e1D.png')
 
 
-st.title("References")
+st.title(":scroll: References")
 
 # Page 3
 st.subheader("Liberia Statistics")
-st.markdown("[World Bank Data catalog](https://datacatalog.worldbank.org/home)")
-st.markdown("[The DHS Program](https://dhsprogram.com/)")
-st.markdown("[The Geospatial Covariate Datasets Manual from DHS Program](https://spatialdata.dhsprogram.com/references/DHS_Covariates_Extract_Data_Description_3.pdf)")
-st.markdown("[Guide to DHS Statistics](https://dhsprogram.com/Data/Guide-to-DHS-Statistics/index.cfm)")
-st.markdown("[Humanitarian Data Exchange(HDX) Liberia Health Facilities](https://data.humdata.org/dataset/hotosm_lbr_health_facilities)")
+st.markdown("1. [World Bank Data catalog](https://datacatalog.worldbank.org/home)")
+st.markdown("2. [The DHS Program](https://dhsprogram.com/)")
+st.markdown("3. [The Geospatial Covariate Datasets Manual from DHS Program](https://spatialdata.dhsprogram.com/references/DHS_Covariates_Extract_Data_Description_3.pdf)")
+st.markdown("4. [Guide to DHS Statistics](https://dhsprogram.com/Data/Guide-to-DHS-Statistics/index.cfm)")
+st.markdown("5. [Humanitarian Data Exchange(HDX) Liberia Health Facilities](https://data.humdata.org/dataset/hotosm_lbr_health_facilities)")
 st.divider()
 
 # Page 4
 st.subheader("Rainfall, Intervention and Malaria statistics in Liberia")
-st.markdown("[Malaria Atlas Project](https://malariaatlas.org/)") 
-st.markdown("[Humanitarian Data Exchange(HDX) Liberia Rainfall Indicators at Subnational Level](https://data.humdata.org/dataset/a1f60b8a-51ff-4ee7-87ab-897983714595)")
+st.markdown("1. [Malaria Atlas Project](https://malariaatlas.org/)") 
+st.markdown("2. [Humanitarian Data Exchange(HDX) Liberia Rainfall Indicators at Subnational Level](https://data.humdata.org/dataset/a1f60b8a-51ff-4ee7-87ab-897983714595)")
 
 st.divider()
 
